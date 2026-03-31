27477d81ef603274bc7748d5649b062ecf78e4d2
arysweb <sebastian.czinna@gmail.com>
2026-03-31 21:07:53 +0200

    fix CORS, update demo page

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const dns = require('dns');
 const { promisify } = require('util');
 
 const app = express();
-app.use(cors({
-  origin: ['https://arysweb.github.io', 'http://localhost:3000']
-}));
+app.use(cors());
 app.use(express.json());
 
 const resolveMx = promisify(dns.resolveMx);
